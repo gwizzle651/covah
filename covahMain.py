c0f1d7828cc5e2936a155e4046b7204de6d1ec6f
gwizzle651 <gwizzle651@proton.me>
2025-09-09 11:50:21 -0500

    created custom strikes system

--- covahMain.py
+++ covahMain.py
@@ -2,16 +2,32 @@ import discord
 from discord.ext import commands, tasks
 import os
 import random
+import json
 
 token = os.getenv("covahBotToken")
 
 intents = discord.Intents.default()
 intents.message_content = True
 intents.members = True
-
 bot = commands.Bot(command_prefix="$", intents=intents)
 
+
+def loadStrikes():
+    try:
+        with open('userStrikes.json', 'r') as f:
+            return json.load(f)
+    except FileNotFoundError:
+        return {}
+
+
+def saveStrikes():
+    with open('userStrikes.json', 'w') as f:
+        json.dump(userStrikes, f)
+
+
+actionLogChannelID = 1252706858192474174
 botCommandsChannelID = 1345050469302665257
+userStrikes = loadStrikes()
 
 
 # events
@@ -32,7 +48,7 @@ async def on_command_error(ctx, error):
     elif isinstance(error, commands.MissingRequiredArgument):
         await ctx.reply("⚠️ You are missing some required argument(s)!")
     elif isinstance(error, commands.errors.CheckFailure):
-        await ctx.send('You do not have the correct role for this command.')
+        await ctx.send("You do not have the correct role for this command.")
 
 
 @bot.event
@@ -40,18 +56,14 @@ async def on_message(message):
     if message.author == bot.user:
         return
 
-    if message.content.lower() == "lol":
-        # await message.channel.send("i know that it is not that funny")
-        await message.reply('i know that it is not that funny')
+    if "lol" in message.content.lower():
+        await message.reply("i know that it is not that funny")
 
-    if message.content.lower() == "bruh":
-        await message.reply('shut up loser')
+    if "bruh" in message.content.lower():
+        await message.reply("shut up loser")
 
-    if message.content == "🦐":
-        await message.reply('🥐 fr*nch 🥖')
-        await message.add_reaction('🇨🇵')
-    if message.content == '🧽':
-        await message.reply('S P O N G U E')
+    if "🧽" in message.content:
+        await message.reply("S P O N G U E")
 
     await bot.process_commands(message)
 
@@ -83,43 +95,46 @@ async def dice(ctx, sides: int, numDice: int):
 @bot.command(name='secret', help='Completely useless.')
 @commands.has_role('Admin')
 async def secret(ctx):
-    await ctx.send(f'{ctx.author.name} activated the secret command!')
+    await ctx.send(f"{ctx.author.name} activated the secret command!")
 
-'''
-@bot.command(name='strike', help='Manages the striking system.')
-@commands.has_any_role('Admin', 'Mod')
+
+@bot.command(name="strike", help="Manages the striking system.")
+@commands.has_any_role("Admin", "Mod")
 async def strike(ctx, mode: str, user: discord.Member, *, reason=None):
-    if mode not in ['add', 'remove']:
+    if mode not in ["add", "remove"]:
         await ctx.send("Invalid mode. Use 'add' to issue a strike or"
                        " 'remove' to remove a strike.")
         return
 
-    if user.id not in user_strikes:
-        user_strikes[user.id] = 0
+    if user.id not in userStrikes:
+        userStrikes[user.id] = 0
     if mode == 'add':
-        user_strikes[user.id] += 1
+        userStrikes[user.id] += 1
         await ctx.send(f"Strike added to {user.name}."
-                       "Total strikes: {user_strikes[user.id]}")
+                       "Total strikes: {userStrikes[user.id]}")
 
-        if user_strikes[user.id] >= 3:
-            await ctx.send(f"{user.name} has reached 3 strikes and"
-                           " will be muted by @Admin or @Mod.")
+        if userStrikes[user.id] >= 3:
+            await ctx.send(f"{user.name} has reached {userStrikes[user.id]}"
+                           " strikes and will be punished by"
+                           " <@&1252708314157023303> or"
+                           " <@&1252703138809380874> accordingly.")
 
     elif mode == 'remove':
-        if user_strikes[user.id] > 0:
-            user_strikes[user.id] -= 1
+        if userStrikes[user.id] > 0:
+            userStrikes[user.id] -= 1
             await ctx.send(f"Strike removed from {user.name}."
-                           " Total strikes: {user_strikes[user.id]}")
+                           " Total strikes: {userStrikes[user.id]}")
         else:
             await ctx.send(f"{user.name} has no strikes to remove.")
 
     if reason:
+        channel = bot.get_channel(actionLogChannelID)
+        await channel.send(f"Reason: {reason}")
         await ctx.send(f"Reason: {reason}")
-'''
 
 
 # automation
-@tasks.loop(hours=1)
+@tasks.loop(hours=2)
 async def bumpServer():
     await bot.wait_until_ready()
     channel = bot.get_channel(botCommandsChannelID)
